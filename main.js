7865a82a57537ebb2d7ef713da9f200a5d58f204
Achmad Rifqi Rosadi <55131961+achmadrifqi09@users.noreply.github.com>
2024-07-10 10:54:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './style.css'
 import {initPWA} from "./pwa.js";
 
 let app  = document.querySelector('#app').innerHTML = `
-  <iframe src="https://ummhospital.com/" width="100%" height="100%" scrolling="auto"></iframe> 
+  <iframe src="https://sim.ummhospital.com/" width="100%" height="100%" scrolling="auto"></iframe> 
 `;
 
 initPWA(app);
